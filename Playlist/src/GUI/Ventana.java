d9b413a30fba5fbb4b2a3c2091760dfef17da852
fersebas05 <sebastianleal.687@gmail.com>
2026-06-03 15:32:34 -0300

    Update Ventana.java

--- Playlist/src/GUI/Ventana.java
+++ Playlist/src/GUI/Ventana.java
@@ -1,5 +1,5 @@
 package GUI;
-//Actualizado el 02 / 06 / 2026
+
 import java.io.File;
 
 import Reproductor.*;
@@ -8,6 +8,9 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
@@ -32,11 +35,16 @@ public class Ventana extends Application{
     protected Button button_ordenarAnio;
 
     protected Button button_aleatorio;
+
+    protected Label labelCancionActual;
+    protected ListView<String> listViewPlaylist;
+
+    protected Slider sliderProgreso;
     
     public void start(Stage stage) {
         inicializarComponentes();
 
-        Scene escena = new Scene(crearLayout(), 500, 300);
+        Scene escena = new Scene(crearLayout(), 600, 400);
 
         stage.setTitle("Reproductor MP3");
         stage.setScene(escena);
@@ -51,6 +59,7 @@ public class Ventana extends Application{
 
         this.button_addFiles = new Button("Add Files");
 
+        
         this.button_prev = new Button("<<");
         this.button_play = new Button("Play");
         this.button_pause = new Button("Pause");
@@ -59,9 +68,16 @@ public class Ventana extends Application{
 
         this.button_aleatorio = new Button("Aleatorio");
 
+        // Botones para ordenar la playlist
         this.button_ordenarTitulo = new Button("Ordenar por Título");
         this.button_ordenarArtista = new Button("Ordenar por Artista");
         this.button_ordenarAnio = new Button("Ordenar por Año");
+
+        // Label y ListView para mostrar la canción actual y la playlist
+        this.labelCancionActual = new Label("Canción actual: ");
+        this.listViewPlaylist = new ListView<>();
+        this.listViewPlaylist.setPrefHeight(150);
+        this.listViewPlaylist.setPrefWidth(400);
 
         registrarEventos();
     }
@@ -86,44 +102,73 @@ public class Ventana extends Application{
                 }
                 
             }
-
+            //this.reproductor.cargarPlayList(this.playlist);
+            actualizarListView();
         });
 
         this.button_prev.setOnAction(e -> {
-            System.out.println("ANTERIOR");
             this.reproductor.anterior();
+            actualizarLabelCanciones();
         });
 
         this.button_play.setOnAction(e -> {
-            System.out.println("PLAY");
             this.reproductor.play();
+            actualizarLabelCanciones();
         });
+        
         this.button_pause.setOnAction(e -> this.reproductor.pause());
         this.button_stop.setOnAction(e -> this.reproductor.stop());
 
         this.button_next.setOnAction(e -> {
             System.out.println("SIGUIENTE");
             this.reproductor.siguiente();
+            actualizarLabelCanciones();
         });
 
         this.button_ordenarTitulo.setOnAction(e -> {
             this.playlist.ordenar(PlayListAbs.OrdenCriterio.TITULO);
             this.reproductor.cargarPlayList(this.playlist);
+            actualizarListView();
         });
         this.button_ordenarArtista.setOnAction(e -> {
             this.playlist.ordenar(PlayListAbs.OrdenCriterio.ARTISTA);
             this.reproductor.cargarPlayList(this.playlist);
+            actualizarListView();
         });
         this.button_ordenarAnio.setOnAction(e -> {
             this.playlist.ordenar(PlayListAbs.OrdenCriterio.ANIO);
             this.reproductor.cargarPlayList(this.playlist);
+            actualizarListView();
         });
         this.button_aleatorio.setOnAction(e -> {
             this.reproductor.toggleAleatorio();
+            actualizarAleatorio();
+            actualizarListView();
         });
+
+        this.listViewPlaylist.setOnMouseClicked(e -> {
+            int indice = listViewPlaylist.getSelectionModel().getSelectedIndex();
+            reproductor.seleccionarCancion(indice);
+            reproductor.play();
+            
+            actualizarListView();
+            actualizarLabelCanciones();
+        });
+
     }
 
     private VBox crearLayout() {
+        HBox addFilesBox = new HBox();
+        addFilesBox.setSpacing(10);
+        addFilesBox.getChildren().add(button_addFiles);
+        addFilesBox.setAlignment(Pos.TOP_LEFT);
+        
+        HBox playlistBox = new HBox();
+        playlistBox.setSpacing(10);
+        playlistBox.setPrefHeight(200);
+        playlistBox.getChildren().add(listViewPlaylist);
+        playlistBox.setAlignment(Pos.CENTER);
+
         HBox controles = new HBox();
         controles.setSpacing(10);
         controles.getChildren().addAll(button_prev,button_play, button_pause, button_stop, button_aleatorio, button_next);
@@ -134,12 +179,41 @@ public class Ventana extends Application{
         ordenar.getChildren().addAll(button_ordenarTitulo, button_ordenarArtista, button_ordenarAnio);
         ordenar.setAlignment(Pos.CENTER);
 
+
         VBox root = new VBox();
         root.setSpacing(20);
-        root.getChildren().addAll(button_addFiles, controles, ordenar);
+        root.getChildren().addAll(addFilesBox, playlistBox, this.labelCancionActual, controles, ordenar);
         root.setAlignment(Pos.CENTER);
 
         return root;
+    }
+
+    private void actualizarListView() {
+        this.listViewPlaylist.getItems().clear();
+
+        for(int i = 0; i < this.playlist.tamanio(); i++) {
+            Cancion cancion = (Cancion) this.playlist.devolver(i);
+            this.listViewPlaylist.getItems().add(cancion.toString());
+        }
+    }
+
+    private void actualizarAleatorio() {
+        if(this.reproductor.esAleatorio()) {
+            button_aleatorio.setText("🔀 ON");
+        }
+        else {
+            button_aleatorio.setText("🔀 OFF");
+        }
+    }
+
+    private void actualizarLabelCanciones() {
+        Cancion cancion = (Cancion) this.reproductor.getCancionActual();
+        if(cancion != null) {
+            this.labelCancionActual.setText("🎶"+ cancion.toString());
+        }
+        else {
+            this.labelCancionActual.setText("Sin reproducción actual");
+        }
     }
 
 }
